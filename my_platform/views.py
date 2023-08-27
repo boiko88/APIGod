2f26f7ccfc0332137033511f7218fb9e16811d2e
Anton Boiko <bo1ko.ant@yandex.com>
2023-08-27 16:37:45 +0300

    added current time, day and month revealed on the time.html page

--- my_platform/views.py
+++ my_platform/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+import calendar
+from datetime import datetime
+
 
 
 def home(request):
@@ -7,8 +9,25 @@ def home(request):
     return render(request, 'home.html', context)
 
 
-def myTime(request):
-    context = {}
+def myTime(request, year=datetime.now().year, month=datetime.now().strftime('%B'), current_day=datetime.now()):
+    month = month.capitalize()
+    month_number = list(calendar.month_name).index(month)
+    month_number = int(month_number)
+    
+    # Get current time
+    now = datetime.now()
+    current_year = now.year
+    current_day = now.day
+    
+    # Cakendar
+    now = datetime.now()
+    now.strftime("%A")
+    
+    context = {
+        'current_year': current_year,
+        'current_day': current_day,
+        'now': now,
+        }
     return render(request, 'time.html', context)
 
 
